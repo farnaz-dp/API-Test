b20fe2626f1576093dc70ee9068e56075a70f491
farnaz <farnaz.dp7394@gmail.com>
2021-02-21 17:24:48 +0330

    Login added to Github

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,43 +1,61 @@
 
 import React from "react";
 import {useState,useEffect} from "react";
+import axios from "axios";
+
 
 const Login = (props) => {
 
-   const [usernameState , setUsernameState] =useState(null)
-   const [passwordState , setPasswordState] =useState(null)
-
-   const handleInputChange = (event) => {
-       // console.log("Event : " , event.target.name)
-
-       if (event.target.type ==="submit" ){
-           console.log("Event type is Submit Button")
-       }
-       else
-       {
-           console.log("Event type is text input ")
-           if (event.target.name === "username"){
-               setUsernameState(event.target.value)
-           }
-       else
-           {
-               setPasswordState(event.target.value)
-           }
-       }
+    const [usernameState , setUsernameState] =useState('')
+    const [passwordState , setPasswordState] =useState('')
+    const [apiFetchState , setApiFetchState] =useState(false)
 
 
+    const handleInputChange = (event) => {
+        // console.log("Event : " , event.target.name)
 
-   }
+        if (event.target.type ==="submit" ){
+            event.preventDefault()
+            if (usernameState != '' & passwordState != ''){
+                axios({
+                    method: 'post',
+                    url: 'http://192.168.100.56/api-token-auth',
+                    data: {
+                        username : usernameState,
+                        password: passwordState
+                    }
 
-   useEffect(()=>{
+                })
+                    .then( (response)=>{
+                        // console.log(`Response : ${response}`)
 
-       console.log("usernameState : " , usernameState)
-   },[usernameState])
+                        if (response.status === 200){
+                            console.log('Login,Response : ' , response.data)
+                            setApiFetchState(true)
+                            props.setApiAuthTokenState(response.data)
+                        }
+                    })
+                    .catch((error)=>{
+                        console.log('Error : ',error)
+                    })
+            }
+            console.log("Event type is Submit Button")
 
-    useEffect(()=>{
+        }
+        else
+        {
+            console.log("Event type is text input ")
+            if (event.target.name === "username"){
+                setUsernameState(event.target.value)
+            }
+            else if (event.target.name === "password")
+            {
+                setPasswordState(event.target.value)
+            }
+        }
 
-        console.log("passwordState : " , passwordState)
-    },[passwordState])
+
+    }
 
     return(
         <div>
@@ -53,9 +71,10 @@ const Login = (props) => {
                 </label>
                 <input type={"password"} name={"password"} value={passwordState} onChange={handleInputChange}/>
                 <br/>
-                <input type={"submit"}/>
+                <input type={"submit"} onClick={handleInputChange}/>
 
             </form>
+
         </div>
     )
 }
