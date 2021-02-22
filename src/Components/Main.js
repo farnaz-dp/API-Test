f29d9f7c4016cdf0b0eb5d21e11adcb13bbc122e
farnaz <farnaz.dp7394@gmail.com>
2021-02-22 11:18:06 +0330

    Main added to Git

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import axios from "axios";
+import {SvgApp} from "./SvgApp";
+import {MoonLoader} from "react-spinners";
+import { css } from "@emotion/react";
+
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const Main = (props)=> {
 
     const [map3dDataState , setMap3dDataState] = useState(null)
+    const [isMap3dDataFetched , setIsMap3dDataFetched] = useState(false)
+    const [svgIsLoaded , setSvgIsLoaded] = useState(false)
 
     useEffect( ()=>{
         axios({
@@ -16,6 +28,7 @@ const Main = (props)=> {
 
         })
             .then((response)=>{
+                setIsMap3dDataFetched(true)
                 setMap3dDataState(response.data)
                 console.log('Main, Response : ',response.data)
             })
@@ -26,9 +39,38 @@ const Main = (props)=> {
 
     }, [])
 
+    const svgShowRender = () =>{
+        if (!svgIsLoaded) {
+            return (
+                <MoonLoader color={'#01b3fd'} loading={true} css={override} size={150} />
+            )
+        }
+        else {
+            return null
+        }
+    }
+
+    const SvgAppShowRender = ()=>{
+        if (isMap3dDataFetched & map3dDataState != null) {
+            return(
+
+                <SvgApp svgUrl={map3dDataState.url} setSvgIsLoaded={setSvgIsLoaded}/>
+            )
+        }
+        else
+        {
+            return null
+        }
+    }
 
     return(
-        <h2>Welcome To Main Page</h2>
+        <div>
+            <h2>Welcome To Main Page</h2>
+            {svgShowRender()}
+            <svg id='svg_file'></svg>
+            {SvgAppShowRender()}
+        </div>
+
     )
 }
 
