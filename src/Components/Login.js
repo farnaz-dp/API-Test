439b590496f80617f1d777db5dd33a9369a3824e
farnaz <farnaz.dp7394@gmail.com>
2021-02-22 11:17:17 +0330

    Login.js is added to Git

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -28,7 +28,6 @@ const Login = (props) => {
                 })
                     .then( (response)=>{
                         // console.log(`Response : ${response}`)
-
                         if (response.status === 200){
                             console.log('Login,Response : ' , response.data)
                             setApiFetchState(true)
