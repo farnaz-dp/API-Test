d439d67cc99956e9e59013135c8691050e6360e8
farnaz <farnaz.dp7394@gmail.com>
2021-02-24 16:18:12 +0330

    ContentContainer added to Git

---
+++ src/Components/Content/ContentContainer.js
@@ -0,0 +1,75 @@
+
+import React from 'react'
+import {useState , useEffect} from 'react'
+import {ContentComponent} from "./ContentComponent";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+
+const ContentContainer = (props)=>{
+
+    const {svgObjectIdClick , tabs} = props
+
+    console.log('ContentContainer , object box id :' , svgObjectIdClick)
+    console.log('ContentContainer , tabs :' , tabs)
+
+    const tabNameRender = ()=>{
+        const contentList = tabs.map((tab , index)=>{
+
+            return (
+
+                        <Tab>
+                            {tab.tab_name}
+                        </Tab>
+
+            )
+
+        })
+        return (
+
+                <TabList>
+                    {contentList}
+                </TabList>
+
+
+        )
+    }
+
+    const tabPannelRender = ()=>{
+        const contentList = tabs.map((tab , index)=>{
+
+            return (
+
+                <TabPanel>
+                    {/*{tab.tab_name} <br/>*/}
+                    {/*{tab.clickable_object}<br/>*/}
+                    {/*{tab.object_id}<br/>*/}
+                    {/*{tab.table_name}*/}
+                    <ContentComponent
+                        sensors={tab.sensors}
+                        table_name={tab.table_name}
+                    />
+                </TabPanel>
+
+            )
+
+        })
+        return contentList
+
+
+    }
+
+
+    return(
+        <div>
+            <Tabs>
+                {tabNameRender()}
+                {tabPannelRender()}
+            </Tabs>
+
+        </div>
+    )
+}
+
+
+export {ContentContainer}
