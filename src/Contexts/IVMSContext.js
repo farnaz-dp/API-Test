43557e4afddcca9dd7efff8ce9a653d036a573f5
farnaz <farnaz.dp7394@gmail.com>
2021-03-13 11:33:53 +0330

    IVMSContext.js added to Git

---
+++ src/Contexts/IVMSContext.js
@@ -0,0 +1,11 @@
+import {createContext} from 'react'
+
+// const IVMSContext = createContext({
+//     state : null,
+//     dispatch : ()=>{}
+//
+// })
+
+const Context = createContext(null)
+
+export {Context}
