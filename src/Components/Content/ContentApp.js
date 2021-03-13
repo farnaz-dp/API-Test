c1ff6f6ba09a1942e11eb2f5e4894cd37cd0f300
farnaz <farnaz.dp7394@gmail.com>
2021-03-13 10:11:36 +0330

    Content App added to Git

--- src/Components/Content/ContentApp.js
+++ src/Components/Content/ContentApp.js
@@ -1,16 +1,22 @@
 
-import React from 'react'
+import React , {useContext} from 'react'
 import {useState , useEffect } from 'react'
 import {ContentContainer} from "./ContentContainer";
+import {Context} from '../../Contexts/IVMSContext'
+
 
 const ContentApp = (props) =>{
 
-    const {svgObjectIdClick, tabs} = props
+    const {state , dispatch} = useContext(Context)
+
+    // const {svgObjectIdClick, tabs} = props
 
     const tabFilter = () =>{
 
-        const tabList = tabs.filter((tab)=>{
-            if (tab.object_id === svgObjectIdClick)
+        // const tabList = tabs.filter((tab)=>{
+        const tabList = state.ivmsMapDataApi.data.table.tabs.filter((tab)=>{
+            // if (tab.object_id === svgObjectIdClick)
+            if (tab.object_id === state.svg.svgObjectIdClick)
             {
                 return tab
             }
@@ -21,8 +27,12 @@ const ContentApp = (props) =>{
 
     return(
         <div>
+            {/*<ContentContainer*/}
+            {/*    svgObjectIdClick={svgObjectIdClick}*/}
+            {/*    tabs={tabFilter()}*/}
+            {/*/>*/}
+
             <ContentContainer
-                svgObjectIdClick={svgObjectIdClick}
                 tabs={tabFilter()}
             />
         </div>
