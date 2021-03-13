5f20c17057baad062f1faf2c9b526e9a5ef949a8
farnaz <farnaz.dp7394@gmail.com>
2021-03-13 13:08:43 +0330

    SvgContainer added to Git

--- src/Components/SVG/SvgContainer.js
+++ src/Components/SVG/SvgContainer.js
@@ -1,25 +1,37 @@
 
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect , useContext} from 'react'
 import {SvgComponent} from "./SvgComponent";
-
+import {Context} from "../../Contexts/IVMSContext";
 
 const SvgContainer = (props) => {
 
+    const {state , dispatch} = useContext(Context)
+
 //Extracts required fields from tabs array
     const svgComponentsRender = ()=> {
-        const componentList = props.tabs.map((tab, index)=>{
+        // const componentList = props.tabs.map((tab, index)=>{
+        const componentList = state.ivmsMapDataApi.data.table.tabs.map((tab, index)=>{
             return(
-                <SvgComponent
-                    tab_name={tab.tab_name}
-                    object_id={tab.object_id}
-                    table_name={tab.table_name}
-                    sensors={tab.sensors}
-                    svgSnap={props.svgSnap}
-                    key={index}
-                    setSvgObjectIdClick={props.setSvgObjectIdClick}
-                    setDrawerVisible={props.setDrawerVisible}
-                    svgObjectIdClick={props.svgObjectIdClick}
-                />
+                // <SvgComponent
+                //     tab_name={tab.tab_name}
+                //     object_id={tab.object_id}
+                //     table_name={tab.table_name}
+                //     sensors={tab.sensors}
+                //     svgSnap={props.svgSnap}
+                //     key={index}
+                //     setSvgObjectIdClick={props.setSvgObjectIdClick}
+                //     setDrawerVisible={props.setDrawerVisible}
+                //     svgObjectIdClick={props.svgObjectIdClick}
+                // />
+
+            <SvgComponent
+                tab_name={tab.tab_name}
+                object_id={tab.object_id}
+                table_name={tab.table_name}
+                sensors={tab.sensors}
+                key={index}
+                svgSnap={props.svgSnap}
+            />
             )
 
         })
