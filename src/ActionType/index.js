fc336b94f554df880d008f3b673ab348ded809a6
farnaz <farnaz.dp7394@gmail.com>
2021-04-10 15:31:18 +0430

    index.js for ActionType added to Git

---
+++ src/ActionType/index.js
@@ -0,0 +1,9 @@
+export {LoginApiRequest ,LoginApiSuccess ,LoginApiError,fetchToken} from './LoginApi/LoginApiActionCreator'
+
+export {MapDataApiRequest ,MapDataApiSuccess,MapDataApiError ,fetchMapData} from './MapDataApi/MapDataActionCreator'
+
+export {SvgLoaded,SvgUnLoaded,SvgClick} from './Svg/SvgActionCreator'
+
+export {ContentShow,ContentNotShow} from './Content/ContentActionCreator'
+
+export {DrawerShow,DrawerNotShow} from './Drawer/DrawerActionCreator'
