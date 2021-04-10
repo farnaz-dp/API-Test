0f8416223e19d10bef1a4f28f608367689bf4f0d
farnaz <farnaz.dp7394@gmail.com>
2021-04-10 15:32:41 +0430

    index.js for Reducers added to Git

---
+++ src/Reducers/index.js
@@ -0,0 +1,9 @@
+export {loginApiReducer} from './LoginApiReducer/LoginApiReducer'
+
+export {mapDataApiReducer} from './MapDataApiReducer/MapDataApiReducer'
+
+export {svgReducer} from './SvgReducer/SvgReducer'
+
+export {contentReducer} from './ContentReducer/ContentReducer'
+
+export {drawerReducer} from './DrawerRedecer/DrawerReducer'
