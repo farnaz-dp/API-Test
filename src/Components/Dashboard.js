79726acd41134ba23266cbc310ea7533a3724287
farnaz <farnaz.dp7394@gmail.com>
2021-05-09 15:33:12 +0430

    Dashboard.js added to Git

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,5 +1,6 @@
-import React  from 'react'
+import React, {useEffect} from 'react'
 import {useSelector,useDispatch} from 'react-redux'
+import {BrowserRouter , Route , Switch} from 'react-router-dom'
 import {Login} from "./Login";
 import {Main} from "./Main";
 
@@ -9,6 +10,15 @@ const Dashboard = (props)=>{
     // const [apiAuthTokenState , setApiAuthTokenState] =useState(null)
 
     const loginApi = useSelector(state => state.loginApi.token)
+    const user = useSelector(state => state.loginApi.user)
+
+    useEffect(() => {
+        if (user) {
+            console.log("Dashboard , token is :" , loginApi)
+            console.log("Dashboard , user is :" , user)
+        }
+
+    } ,[loginApi , user])
 
     const pageRender = () => {
         if (loginApi === null){
